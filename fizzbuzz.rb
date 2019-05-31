e5822d7aa51ee41e14cc58bf5dc518af0d28c2aa
Adrian Popan <adrian.popan@gmail.com>
2019-05-31 20:16:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 3 == 0 
+    return 'fizz'
+  elsif num % 5 == 0 
+    return 'buzz'
+  elsif num % 15 == 0 
+    return 'fizzbuzz'
+  else num % 4
+    return
+  end
+end
